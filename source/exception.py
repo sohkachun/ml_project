8bd5b60d4d4868c820f3e1b8cc69494795c3b087
sohkachun <sohkachun@yahoo.com>
2024-07-26 08:34:49 -0400

    Data Ingestion

--- source/exception.py
+++ source/exception.py
@@ -1,5 +1,5 @@
 import sys 
-import logging
+from source.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -17,3 +17,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+if __name__=="__main__":
+    try: 
+        a = 1/0
+    except Exception as e:
+        logging.error("Divide by Zero", exc_info=True)
+        raise CustomException(e, sys)
